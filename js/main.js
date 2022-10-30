b464505939656b942d30dc14580d060f8356d9fb
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-10-29 22:28:31 -0500

    Se inicia repositorio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let button1 = document.getElementById("linkedin");
+let button2 = document.getElementById("github");
+
+button1.addEventListener("click", function(){
+    location.href= "https://www.linkedin.com/in/osvaldoresendiz/";
+});//addEventListener button1
+
+button2.addEventListener("click", function(){
+    location.href= "https://github.com/OsvaldoReTo";
+});//addEventListener button1
