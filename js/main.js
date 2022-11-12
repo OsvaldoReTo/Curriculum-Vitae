05670358c9ef473fade51405bb80550531d8df0e
Osvaldo Resendiz Tolentino <resendiz.tolentino.osvaldo@gmail.com>
2022-11-11 19:57:15 -0600

    Se completa primera versión de página

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let button1 = document.getElementById("linkedin");
 let button2 = document.getElementById("github");
+let articulo = document.getElementById("articulo");
+let mexishop = document.getElementById("mexishop");
 
 button1.addEventListener("click", function(){
     location.href= "https://www.linkedin.com/in/osvaldoresendiz/";
@@ -8,3 +10,11 @@ button1.addEventListener("click", function(){
 button2.addEventListener("click", function(){
     location.href= "https://github.com/OsvaldoReTo";
 });//addEventListener button1
+
+articulo.addEventListener("click", function(){
+    location.href="https://doi.org/10.1016/j.jnnfm.2022.104902"
+})
+
+mexishop.addEventListener("click", function(){
+    location.href="https://mexishop-e6.herokuapp.com/"
+})
